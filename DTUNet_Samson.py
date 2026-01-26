dcca7915b0b9c202cfcb4ffa31b4577bf045267c
ChentongWang <155599625+ChentongWang@users.noreply.github.com>
2026-01-26 16:55:34 +0800

    Update DTUNet_Samson.py

--- DTUNet_Samson.py
+++ DTUNet_Samson.py
@@ -89,49 +89,6 @@ class ChannelAttention(nn.Module):
         return x
 
 
-class CharbonnierLoss(nn.Module):
-    """Charbonnier Loss (L1)"""
-
-    def __init__(self, eps=1e-3):
-        super(CharbonnierLoss, self).__init__()
-        self.eps = eps
-
-    def forward(self, x, y):
-        diff = x - y
-        # loss = torch.sum(torch.sqrt(diff * diff + self.eps))
-        loss = torch.mean(torch.sqrt((diff * diff) + (self.eps*self.eps)))
-        return loss
-
-class EdgeLoss(nn.Module):
-    def __init__(self):
-        super(EdgeLoss, self).__init__()
-        k = torch.Tensor([[.05, .25, .4, .25, .05]])
-        self.kernel = torch.matmul(k.t(), k).unsqueeze(0).repeat(3, 1, 1, 1)
-        if torch.cuda.is_available():
-            self.kernel = self.kernel.cuda()
-        self.loss = CharbonnierLoss()
-        self.downsampling22 = nn.AvgPool2d(2, 2,ceil_mode =True)
-
-    def conv_gauss(self, img):
-        n_channels, _, kw, kh = self.kernel.shape
-        img = F.pad(img, (kw//2, kh//2, kw//2, kh//2), mode='replicate')
-        return F.conv2d(img, self.kernel, groups=n_channels)
-
-    def laplacian_kernel(self, current):
-        filtered    = self.conv_gauss(current)    # filter
-        down        = filtered[:,:,::2,::2]               # downsample
-        new_filter  = torch.zeros_like(filtered)
-        new_filter[:,:,::2,::2] = down*4                  # upsample
-        filtered    = self.conv_gauss(new_filter) # filter
-        diff = current - filtered
-        return diff
-
-    def forward(self, x, y):
-        x = self.downsampling22(x)
-        loss = self.loss(self.laplacian_kernel(x), self.laplacian_kernel(y))
-        return loss
-
-
 # abundance normalization
 def norm_abundance_GT(abundance_input, abundance_GT_input):
     abundance_input = abundance_input / (torch.sum(abundance_input, dim=1))
@@ -876,7 +833,6 @@ train_loader = torch.utils.data.DataLoader(
 )
 
 net = multiStageUnmixing().cuda()
-edgeLoss = EdgeLoss()
 
 # weight init
 net.apply(net.weights_init)
